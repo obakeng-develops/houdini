c02d06a4cc804fa2b71ea10b5b24301b2b2d1201
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2024-07-01 15:02:05 +0200

    Add description for playground command

--- cmd/playground.go
+++ cmd/playground.go
@@ -13,13 +13,9 @@ import (
 // playgroundCmd represents the playground command
 var playgroundCmd = &cobra.Command{
 	Use:   "playground",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Playground is a test command to try out cobra",
+	Long: `
+	houdini playground --path=.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("playground called")
 	},
@@ -28,13 +24,4 @@ to quickly create a Cobra application.`,
 func init() {
 	rootCmd.AddCommand(playgroundCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// playgroundCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// playgroundCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
