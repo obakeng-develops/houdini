b05146f317dc1fd2f04c0c56de1b55cd4710733b
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2024-07-03 12:27:46 +0200

    Update the build command description

--- cmd/build.go
+++ cmd/build.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -13,13 +12,13 @@ import (
 // buildCmd represents the build command
 var buildCmd = &cobra.Command{
 	Use:   "build",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Build will read the given file path and build an image",
+	Long: `
+	# This command builds the image based on the given file path
+	houdini build -i --path=path/to/app -t my-app
+	
+	# This command builds the file path/repo given a tag by the user as well as provision containers
+	houdini build --path=path/to/app -t my-app`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("build called")
 	},
