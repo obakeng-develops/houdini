103ea144a9dbfd8dd86215ae52ece1c95470051c
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2024-07-13 10:42:20 +0200

    Load env file from main

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 /*
 Copyright © 2024 Obakeng Mosadi mosadiobakeng7@gmail.com
-
 */
 package main
 
-import "github.com/obakeng-develops/houdini/cmd"
+import (
+	"log/slog"
+
+	"github.com/joho/godotenv"
+	"github.com/obakeng-develops/houdini/cmd"
+)
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		slog.Error("Error loading .env file", "err", err)
+	}
+
 	cmd.Execute()
 }
