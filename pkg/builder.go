69585ec5073194e1086074488be5c47d7f015120
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2024-07-15 13:26:54 +0200

    Did this all wrong, so removal

--- pkg/builder.go
+++ pkg/builder.go
@@ -1,48 +1,12 @@
 package pkg
 
-import (
-	"context"
-	"fmt"
-	"log/slog"
-	"os"
-
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/client"
-	"github.com/joho/godotenv"
-)
-
-type ImageBuild struct {
-	tag      string
-	language string
-	user     string
-	command  []string
+type BuildOptions struct {
+	Tag      string
+	Language string
+	Path     string
 }
 
-func BuildImage(i *ImageBuild) {
-
-}
-
-func DockerExample() error {
-	err := godotenv.Load()
-	if err != nil {
-		slog.Error("Error loading .env file", "err", err)
-	}
-
-	dockerSocketPath := os.Getenv("DOCKER_SOCKET_PATH")
-
-	cli, err := client.NewClientWithOpts(client.WithHost(dockerSocketPath))
-	if err != nil {
-		panic(err)
-	}
-
-	images, err := cli.ImageList(context.TODO(), types.ImageListOptions{})
-	if err != nil {
-		panic(err)
-	}
-
-	for _, ctr := range images {
-		fmt.Printf("%s %s\n", ctr.ID, ctr.Size)
-	}
+func (b *BuildOptions) GenerateImage() error {
 
 	return nil
 }
