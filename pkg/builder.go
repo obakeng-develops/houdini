6c06d46a11a0ccd1ed55794dc1d61e51336a60a7
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2024-08-21 09:01:25 +0200

    Add ImageBuilder interface + Generate Docker function

--- pkg/builder.go
+++ pkg/builder.go
@@ -1,12 +1,30 @@
 package pkg
 
+import (
+	"log"
+	"os"
+)
+
+type ImageBuilder interface {
+	buildImage(f *os.File)
+}
+
 type BuildOptions struct {
 	Tag      string
 	Language string
 	Path     string
 }
 
-func (b *BuildOptions) GenerateImage() error {
+func (b *BuildOptions) GenerateDockerImage() error {
+	f, err := os.OpenFile("Dockerfile", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	switch {
+	case b.Language == "Go":
+		b.buildImage(f)
+	}
 
 	return nil
 }
