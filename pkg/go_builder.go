22d994f1f9cec21053fee24eadf605998d633d3b
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2024-08-21 09:28:22 +0200

    Update go builder

--- pkg/go_builder.go
+++ pkg/go_builder.go
@@ -11,15 +11,11 @@ type GoBuilder struct {
 func (g *GoBuilder) buildImage(f *os.File) {
 	buildGo := `FROM golang:1.19
 
-# Set destination for COPY
 WORKDIR /app
 
-# Download Go modules
 COPY go.mod go.sum ./
 RUN go mod download
 
-# Copy the source code. Note the slash at the end, as explained in
-# https://docs.docker.com/reference/dockerfile/#copy
 COPY *.go ./
 `
 
